20d6df76918568118787791702104ed101b0183d
Utpal Maity <utpal.m@appseconnect.com>
2024-05-10 17:41:08 +0530

    Initial module implemented

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    //config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    //config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddCarter();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapCarter();
+
+app.Run();
