2e3284aa67174b3843f9c6ca1a8fe793403e94ad
Utpal Maity <utpal.m@appseconnect.com>
2024-05-12 12:13:41 +0530

    catalog api completed

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,12 +1,10 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
+﻿
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -18,6 +16,11 @@ namespace Catalog.API.Products.CreateProduct
                 ImageFile = command.ImageFile,
                 Price = command.Price
             };
+
+            //save to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
             //return result
             return new CreateProductResult(product.Id);
         }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -9,6 +9,10 @@ builder.Services.AddMediatR(config =>
     //config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     //config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 builder.Services.AddCarter();
 
